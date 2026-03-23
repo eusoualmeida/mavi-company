0059af2c233f94a21cd9f6d59acb55d58cba6202
💻 Mateus de Almeida <contato.almeidagf@gmail.com>
2026-03-22 22:11:32 -0300

    remove tech section

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <Solutions />
         <HowItWorks />
         <Benefits />
-        <Technologies />
+        {/* <Technologies /> */}
         <TargetAudience />
         <CTA />
         <Footer />
